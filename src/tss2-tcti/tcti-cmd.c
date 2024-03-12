c5a8523162382508a36d16e70b17febff134b7be
Jason King <jason.brian.king@gmail.com>
2024-03-12 13:54:15 +0000

    Placeholder for set_exit_with_parent (needs work)

--- src/tss2-tcti/tcti-cmd.c
+++ src/tss2-tcti/tcti-cmd.c
@@ -18,7 +18,7 @@
 
 #if defined (__FreeBSD__)
 #include <sys/procctl.h>
-#else
+#elif defined (__linux__)
 #include <sys/prctl.h>
 #endif
 #include <sys/types.h>
@@ -192,10 +192,16 @@ static int set_exit_with_parent (void)
     const int sig = SIGTERM;
     return procctl (P_PID, 0, PROC_PDEATHSIG_CTL, (void *)&sig);
 }
-#else
+#elif defined (__linux__)
 static int set_exit_with_parent (void)
 {
     return prctl (PR_SET_PDEATHSIG, SIGTERM);
+}
+#elif defined (__illumos__)
+static int set_exit_with_parent (void)
+{
+    /* TODO */
+    return 0;
 }
 #endif
 
